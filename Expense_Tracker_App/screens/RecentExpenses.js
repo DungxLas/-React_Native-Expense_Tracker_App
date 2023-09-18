30f720655793a07a31e3a105a38246a5ff565e1b
DungxLas <phamjhungfdungx@gmail.com>
2023-09-18 16:21:37 +0700

    18/09/2023

--- Expense_Tracker_App/screens/RecentExpenses.js
+++ Expense_Tracker_App/screens/RecentExpenses.js
@@ -1,17 +1,26 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expenses-context";
-import { getDateMinusDays } from '../ultil/date';
+import { getDateMinusDays } from "../ultil/date";
+import { fetchExpenses } from "../ultil/http";
 
 function RecentExpenses() {
   const expensesCtx = useContext(ExpensesContext);
+
+  useEffect(() => {
+    async function getExpenses() {
+      const expenses = await fetchExpenses();
+    }
+
+    getExpenses();
+  }, []);
 
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();
     const date7DaysAgo = getDateMinusDays(today, 7);
 
-    return (expense.date >= date7DaysAgo) && (expense.date <= today);
+    return expense.date >= date7DaysAgo && expense.date <= today;
   });
 
   return (
